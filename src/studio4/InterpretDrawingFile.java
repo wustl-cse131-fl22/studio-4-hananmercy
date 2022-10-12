b5c235df5e10dc60b9b4ec0fa3791f420c38d83a
colehanan <colehanan@localhost>
2022-10-12 15:07:59 -0500

    tyui

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -21,7 +21,7 @@ public class InterpretDrawingFile {
 		Scanner in = new Scanner(f); // making Scanner with a File
 
 		String shapeType = in.next();
-				
+
 		int redComponent = in.nextInt();
 		int greenComponent = in.nextInt();
 		int blueComponent = in.nextInt();
